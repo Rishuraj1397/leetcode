adaeffc1dd937129ce2826ab50229e21b7a11fc3
Rishu Raj <143050346+Rishuraj1397@users.noreply.github.com>
2026-03-17 04:16:07 +0530

    Time: 3 ms (12.19%), Space: 22.6 MB (79.59%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,6 +1,59 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+    //     vector<int> temp;
+
+    //     for(int i = 0; i < nums.size(); i++) {
+
+    //         bool found = false;
+
+    //         for(int j = 0; j < temp.size(); j++) {
+    //             if(nums[i] == temp[j]) {
+    //                 found = true;
+    //                 break;
+    //             }
+    //         }
+
+    //         if(!found) {
+    //             temp.push_back(nums[i]);
+    //         }
+    //     }
+
+    //     for(int i = 0; i < temp.size(); i++) {
+    //         nums[i] = temp[i];
+    //     }
+
+    //     return temp.size();
+    // } 
+
+
+    //Above solution is a brute force approach
+
+
+
+
+
+
+    //   vector<int> temp;
+
+    //     temp.push_back(nums[0]);
+
+    //     for(int i = 1; i < nums.size(); i++) {
+    //         if(nums[i] != nums[i-1]) {
+    //             temp.push_back(nums[i]);
+    //         }
+    //     }
+
+    //     for(int i = 0; i < temp.size(); i++) {
+    //         nums[i] = temp[i];
+    //     }
+
+    //     return temp.size();
+    // }
+
+
+//this is slightly better appraoch but it uses some extra space
+
         if (nums.size() == 0){
             return 0;//this is edge case 
         }
