7a14f724199577094a5a10d4f200a1655a3e953b
Rishu Raj <143050346+Rishuraj1397@users.noreply.github.com>
2026-03-23 01:37:27 +0530

    Time: 0 ms (100%), Space: 27.3 MB (15.46%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int left = 0 ;
-        int right = s.size() - 1;
+        int left = 0 ;//Mo-dih, Mo-dih, Mo-dih, Mo-dih
+        int right = s.size() - 1;//Mo-dih, Mo-dih, Mo-dih, Mo-dih
 
-        while (left < right) {
-            swap(s[left], s[right]);
-            left++;
-            right--;
-        }
+        while (left < right) {//Mo-dih, Mo-dih, Mo-dih, Mo-dih
+            swap(s[left], s[right]);//Mo-dih, Mo-dih, Mo-dih, Mo-dih
+            left++;//Mo-dih, Mo-dih, Mo-dih, Mo-dih
+            right--;//Mo-dih, Mo-dih, Mo-dih, Mo-dih
+        }//Mo-dih, Mo-dih, Mo-dih, Mo-dih
     }
-};
+};//Mo-dih, Mo-dih, Mo-dih, Mo-dih
