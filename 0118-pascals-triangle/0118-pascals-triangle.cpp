caffc121843a92619ec15d4c8e253952f7f414a6
Rishu Raj <143050346+Rishuraj1397@users.noreply.github.com>
2026-03-22 21:47:43 +0530

    Time: 1 ms (18.76%), Space: 9.7 MB (62.42%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> result;
+        vector<vector<int>> result;//this is for storing result in triangle form
 
-        result.push_back({1});
+        result.push_back({1});//yeah harek me first element push krega like n kuch v ho n = 0 me to sirf 1 hee hoga
 
         for(int row = 1; row < numRows; row++){
-            vector<int> newRow;
-            newRow.push_back(1);
+            vector<int> newRow;//yeah eak new row banayega hareak itteration ke liye
+            newRow.push_back(1);//for every row the forst element should be 1
 
-            vector<int> prevRow = result[row - 1];
+            vector<int> prevRow = result[row - 1];//this is the logic jisme previous row ko store krenge kyuki ussi ko add krke to next row banana hai
 
             for(int i = 1; i < row; i++){
                 newRow.push_back(prevRow[i] + prevRow[i - 1]);
             }
 
-            newRow.push_back(1);
+            newRow.push_back(1);//this is for ending because harek row end v 1 se hee hoga
             result.push_back(newRow);
         }
 
